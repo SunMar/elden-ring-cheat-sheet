e7bf27ae3732008524e7aa145c335874a5c8d33f
SunMar <marijn@suninet.org>
2022-05-19 18:53:38 +0200

    Episode 1

--- javascript.js
+++ javascript.js
@@ -1,67 +1,126 @@
+const language = {
+    'items': {
+        'types': {
+            'gesture': 'Gesture',
+            'multiplayer': 'Multiplayer Item',
+            'grace': 'Site of Lost Grace',
+            'boss': 'Boss',
+            'npc': 'NPC',
+            'rune': 'Rune',
+            'key': 'Key Item',
+            'upgrade': 'Upgrade Material',
+            'consumable': 'Consumable Item',
+            'tool': 'Tool',
+        },
+        'areas': {
+            'chapel-of-anticipation': 'Chapel of Anticipation',
+            'stranded-graveyard': 'Stranded Graveyard',
+            'limgrave-west': 'Limgrave (West)',
+        }
+    },
+    'sections': {
+        'stranded-graveyard': 'Stranded Graveyard',
+        'limgrave-west': 'Limgrave (West)',
+    },
+    'misc': {
+        'done': 'DONE',
+    }
+}
+
 const storage = {
     'section': store.namespace('ercs.section'),
     'checklist': store.namespace('ercs.checklist'),
 };
 
-function createSection(id, name, show) {
-    let accordionClass;
+function createSection(sectionId) {
+    const accordionItem = $(`<div class="accordion-item cs-section" id="${sectionId}"></div>`);
 
-    if (show) {
-        accordionClass = {
-            'header': '',
-            'body': 'show',
-        }
-    } else {
-        accordionClass = {
-            'header': 'collapsed',
-            'body': '',
-        }
-    }
+    const accordionHeader = $(`<h3 class="accordion-header" id="${sectionId}-header"></h3>`);
 
-    return $(`
-        <div class="accordion-item">
-            <h3 class="accordion-header" id="${id}-label">
-                <button class="accordion-button ${accordionClass.header}" type="button" data-bs-toggle="collapse" data-bs-target="#${id}" aria-expanded="true" aria-controls="${id}">
-                    ${name}
-                    <span class="ercs-progress mx-2 px-2 border border-2 rounded-pill text-light"><span id="${id}-done"></span> / <span id="${id}-total"></span></span>
-                </button>
-            </h3>
-            <div id="${id}" class="accordion-collapse collapse ${accordionClass.body}" aria-labelledby="${id}-label">
-                <div class="accordion-body"></div>
-            </div>
+    const accordionButton = $(`
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#${sectionId}-body" aria-expanded="false" aria-controls="${sectionId}-body">
+            ${language.sections[sectionId]}
+            <span class="cs-progress border-danger bg-danger mx-2 px-2 border border-2 rounded-pill text-light">
+                <span id="${sectionId}-done"></span> / <span id="${sectionId}-total"></span>
+            </span>
+        </button>
+    `);
+
+    const accordionButtonDone = $(`<span class="cs-done border-success bg-success mx-2 px-2 border border-2 rounded-pill text-light">${language.misc.done}</span>`);
+    accordionButtonDone.hide();
+
+    accordionButton.append(accordionButtonDone);
+    accordionHeader.append(accordionButton);
+
+    const accordionBody = $(`
+        <div id="${sectionId}-body" class="accordion-collapse collapse" aria-labelledby="${sectionId}-header">
+            <div class="accordion-body"></div>
         </div>
     `);
-}
 
-function createItem(id, name) {
-    return $(`
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="${id}">
-            <label class="form-check-label" for="${id}">${name}</label>
-        </div>
-    `);
-}
+    accordionItem.on('show.bs.collapse', {'sectionId': sectionId}, (event) => {
+        storage.section.set(event.data.sectionId, true);
+    });
 
-function itemChanged(event) {
-    storage.checklist.set(this.id, this.checked);
+    accordionItem.on('hide.bs.collapse', {'sectionId': sectionId}, (event) => {
+        storage.section.set(event.data.sectionId, false);
+    });
 
-    if (this.checked) {
-        event.data.item.find('label').addClass('ecrs-done');
-        sectionIncrementDone(event.data.sectionId);
-    } else {
-        event.data.item.find('label').removeClass('ecrs-done');
-        sectionDecreaseDone(event.data.sectionId);
+    if (storage.section.get(sectionId)) {
+        accordionBody.addClass('show');
+        accordionButton.removeClass('collapsed');
+        accordionButton.attr('aria-expanded', true);
     }
+
+    accordionItem.append(accordionHeader, accordionBody);
+
+    return accordionItem;
 }
 
-function sectionShow(event) {
-    console.log('show', event.data.sectionId);
-    storage.section.set(event.data.sectionId, true);
-}
+function createCheckbox(sectionId, checkboxId, loot, items) {
+    const input = $(`<input class="form-check-input" type="checkbox" id="${checkboxId}">`);
+    const label = $(`<label class="form-check-label" for="${checkboxId}"></label>`);
 
-function sectionHide(event) {
-    console.log('hide', event.data.sectionId);
-    storage.section.set(event.data.sectionId, false);
+    input.on('change', {'sectionId': sectionId, 'checkboxId': checkboxId, 'input': input, 'label': label}, (event) => {
+        const checked = input.prop('checked');
+
+        storage.checklist.set(checkboxId, checked);
+
+        if (checked) {
+            event.data.label.addClass('cs-done');
+            sectionIncrementDone(event.data.sectionId);
+        } else {
+            event.data.label.removeClass('cs-done');
+            sectionDecreaseDone(event.data.sectionId);
+        }
+    });
+
+    const itemList = [];
+
+    $.each(loot.items, (key, value) => {
+        let count, item;
+
+        if (typeof key === "string") {
+            count = value + 'x ';
+            item = key;
+        } else {
+            count = '';
+            item = value;
+        }
+
+        itemList.push(`<a href="${items[item].wiki}" target="_blank">${count} ${items[item].name} (${language.items.types[items[item].type]})</a>`);
+    });
+
+    label.html(`
+        ${itemList.join(', ')}: ${loot.location}
+        <a href="${loot.guide}" target="_blank"><i class="bi bi-play-btn"></i></a>
+        <span class="fst-italic">${language.items.areas[loot.area]}</span>
+    `);
+
+    const checkbox = $(`<div class="form-check"></div>`);
+    checkbox.append(input, label);
+
+    return checkbox;
 }
 
 function sectionDone(sectionId) {
@@ -85,61 +144,61 @@ function sectionUpdateDone(sectionId, done) {
 
     sectionDoneElement.html(done);
 
-    const progress = $(`#${sectionId}-label .ercs-progress`);
-
     if (done === parseInt($(`#${sectionId}-total`).html(), 10)) {
-        progress.addClass([ 'border-success', 'bg-success' ]);
-        progress.removeClass([ 'border-danger', 'bg-danger' ]);
+        $(`#${sectionId}-header .cs-progress`).hide();
+        $(`#${sectionId}-header .cs-done`).show();
     } else {
-        progress.addClass([ 'border-danger', 'bg-danger' ]);
-        progress.removeClass([ 'border-success', 'bg-success' ]);
+        $(`#${sectionId}-header .cs-progress`).show();
+        $(`#${sectionId}-header .cs-done`).hide();
     }
 }
 
-$(window).on('load', function () {
+$(window).on('load', () => {
+    $('#resetChecklist').on('click', () => {
+        $('.cs-section').each((index, element) => {
+            $(`#${element.id}-body`).collapse('hide');
+            sectionUpdateDone(element.id, 0);
+        });
+
+        storage.section.clearAll();
+        storage.checklist.clearAll();
+
+        $('#resetChecklistModal').modal('hide');
+    });
+
     $.ajax("eldenring.json", {
-        'dataType': 'json'
-    }).done(function (response) {
+        'dataType': 'json',
+        'cache': false,
+    }).done((response) => {
         const checklist = $('#checklist');
 
-        $.each(response, function (sectionName, items) {
-            const sectionId = sectionName.replace(/\W/g, '');
-            const section = createSection(sectionId, sectionName, storage.section.get(sectionId));
-            section.on('show.bs.collapse', { 'sectionId': sectionId }, sectionShow);
-            section.on('hide.bs.collapse', { 'sectionId': sectionId }, sectionHide);
+        $.each(response.checkboxes, (sectionId, sectionList) => {
+            const section = createSection(sectionId);
 
             checklist.append(section);
 
-            let doneCounter = 0;
-            let totalCounter = 0;
+            const counters = {
+                'done': 0,
+                'total': 0,
+            }
 
-            $.each(items, function (itemId, itemName) {
-                const item = createItem(itemId, itemName);
-                const checkbox = item.find('input[type=checkbox]');
-                const done = !!storage.checklist.get(checkbox.prop('id'));
+            $.each(sectionList, (index, loot) => {
+                const checkboxId = sectionId + '-' + index;
+                const checkbox = createCheckbox(sectionId, checkboxId, loot, response.items);
+                const done = !!storage.checklist.get(checkboxId);
 
-                totalCounter++;
+                counters.total++;
 
                 if (done) {
-                    doneCounter++;
+                    counters.done++;
+                    checkbox.find(`#${checkboxId}`).prop('checked', done).trigger('change');
                 }
 
-                section.find('.accordion-body').append(item);
-
-                item.find('.form-check-input').on(
-                    'change',
-                    {
-                        'sectionId': sectionId,
-                        'item': item,
-                    },
-                    itemChanged
-                );
-
-                checkbox.prop('checked', done).trigger('change');
+                section.find('.accordion-body').append(checkbox);
             });
 
-            section.find(`#${sectionId}-total`).html(totalCounter);
-            sectionUpdateDone(sectionId, doneCounter);
+            section.find(`#${sectionId}-total`).html(counters.total);
+            sectionUpdateDone(sectionId, counters.done);
         });
     });
 });
